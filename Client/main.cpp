48a936daedd8bf47d3f82964274129e1fbc15768
Jonathan <johnnyswan2005@yahoo.com.au>
2026-02-19 13:26:47 +1100

    Initial commit

---
+++ Client/main.cpp
@@ -0,0 +1,74 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <iostream>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <cstdio>
+#include <thread>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+struct Message{
+    char buffer[200]{};
+    char user[50]{};
+};
+
+int handleClient(SOCKET clientSocket, char username[]){
+    Message msg;
+    strncpy(msg.user, username, sizeof(msg.user));
+    msg.user[sizeof(msg.user) - 1] = '\0';
+    std::cout << username << std::endl;
+    std::cout << "Enter your message ";
+    std::cin.getline(msg.buffer, 200);
+    int byteCount = send(clientSocket, (char*)&msg, sizeof(msg),0);
+    if (byteCount == SOCKET_ERROR) {
+       return -1;
+    }
+    return 0;
+};
+
+int main() {
+    WSADATA wsaData;
+    int wsaerr;
+    WORD wVersionRequested = MAKEWORD(2,2);
+    wsaerr = WSAStartup(wVersionRequested, &wsaData);
+    if (wsaerr !=0) {
+        std::cout << "The Winsock dll not found" << std::endl;
+        return 0;
+    }
+
+    SOCKET clientSocket = INVALID_SOCKET;
+    clientSocket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cout << "Error at socket(): " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 0;
+    }
+
+
+    sockaddr_in clientService;
+    clientService.sin_family = AF_INET;
+    InetPton(AF_INET, ("127.0.0.1"),&clientService.sin_addr.s_addr);
+    clientService.sin_port = htons(8080);
+    if (connect(clientSocket, (SOCKADDR*)&clientService,sizeof(clientService))==SOCKET_ERROR) {
+        std::cout << "connect failed" << std::endl;
+        WSACleanup();
+    }
+;
+    char username[200];
+    std::cout << "What is your username" << std::endl;
+    std::cin >> username;
+    while (true) {
+        int check = handleClient(clientSocket,username);
+        if (check == -1) {
+            break;
+        }
+    }
+    WSACleanup();
+    closesocket(clientSocket);
+}
+
---
+++ Server/main.cpp
@@ -0,0 +1,95 @@
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <iostream>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <cstdio>
+#include <thread>
+#include <chrono>
+#include <vector>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+std::mutex vecMtx;
+
+struct Message{
+    char buffer[200]{};
+    char user[50]{};
+};
+
+void printReceivedMessage(SOCKET acceptSocket, std::vector<SOCKET> currentConnections) {
+    while (true) {
+        Message recievedMsg;
+        int rec = recv(acceptSocket, (char*)&recievedMsg,sizeof(recievedMsg),0);
+        if (rec == SOCKET_ERROR) {
+            std::lock_guard lck(vecMtx);
+            std::erase(currentConnections,acceptSocket);
+            return;
+        }
+        std::cout << recievedMsg.user << "| " << recievedMsg.buffer << std::endl;
+    }
+};
+
+void printCurrentConnection(std::vector<SOCKET> &currentConnections) {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        std::cout << "Current connections " << currentConnections.size() << std::endl;
+    }
+}
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    WSADATA wsaData;
+    int wsaerr;
+    WORD wVersionRequested = MAKEWORD(2,2);
+    wsaerr = WSAStartup(wVersionRequested, &wsaData);
+    if (wsaerr != 0) {
+        std::cout << "Winsock dll not found" << std::endl;
+        return 0;
+    }
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cout << "Error creating server socket" << std::endl;
+        WSACleanup();
+        return 0;
+    }
+
+    sockaddr_in service;
+    service.sin_family = AF_INET;
+    InetPton(AF_INET, "127.0.0.1", &service.sin_addr.s_addr);
+    service.sin_port = htons(8080);
+    if (bind(serverSocket,(SOCKADDR*)&service,sizeof(service))==SOCKET_ERROR) {
+        std::cout << "bind failed" << WSAGetLastError() << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 0;
+    }
+
+    if (listen(serverSocket,5) == SOCKET_ERROR) {
+        std::cout << "listen failed" << std::endl;
+    }
+
+    std::vector<SOCKET> currentConnections;
+    std::thread curConnectionThread(printCurrentConnection, std::ref(currentConnections));
+    curConnectionThread.detach();
+    while (true) {
+        SOCKET acceptSocket = accept(serverSocket,nullptr,nullptr);
+        if (acceptSocket == INVALID_SOCKET) {
+            std::cout << "accept failed" << std::endl;
+            closesocket(serverSocket);
+            closesocket(acceptSocket);
+            WSACleanup();
+            return 0;
+        }
+        currentConnections.push_back(acceptSocket);
+        std::thread recvThread(printReceivedMessage,acceptSocket, std::ref(currentConnections));
+        recvThread.detach();
+    }
+}
+
+
+
